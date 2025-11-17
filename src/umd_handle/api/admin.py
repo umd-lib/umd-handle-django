2d5506e158f1dcefbd51f0be9fb242bf4aabb5d6
David P. Steelman <dsteelma@umd.edu>
2025-11-17 15:19:01 -0500

    LIBITD-2612. Rename "HandleModelAdmin" to "HandleAdmin", add list view
    Renamed the "HandleModelAdmin" class to "HandleAdmin", as that seemed
    more consistent with Django conventions.
    
    Added a list view showing the following fields (matching what was in
    the list view in the legacy Rails-based "umd-handle" application):
    
    * Prefix
    * Suffix
    * Url
    * repo
    * repo_id
    
    Set default ordering for admin list to be "modified" descending.
    
    https://umd-dit.atlassian.net/browse/LIBITD-2612

--- src/umd_handle/api/admin.py
+++ src/umd_handle/api/admin.py
@@ -2,11 +2,17 @@ from django.contrib import admin
 
 from .models import Handle
 
-class HandleModelAdmin(admin.ModelAdmin):
+class HandleAdmin(admin.ModelAdmin):
     fields = [
         'prefix', 'suffix', 'url', 'repo', 'repo_id', 'description', 'notes',
         'created', 'modified',
     ]
     readonly_fields = ['created', 'modified']
 
-admin.site.register(Handle, HandleModelAdmin)
+    list_display = (
+        'prefix', 'suffix', 'url', 'repo', 'repo_id', 'modified'
+    )
+    # Default order for admin list - modified descending
+    ordering = ['-modified']
+
+admin.site.register(Handle, HandleAdmin)
